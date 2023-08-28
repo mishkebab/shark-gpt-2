e8a316f793388b3730bf4c544e936f9723133d30
misha <mishabansal7@gmail.com>
2023-08-28 09:33:22 -0400

    fixing response object

--- frontend/src/scripts/screen3.js
+++ frontend/src/scripts/screen3.js
@@ -50,6 +50,11 @@ export function renderScreen3(pitch, category, renderScreen4) {
     let warren;
     let mark;
     let serena;
+    let elon_score;
+    let beyonce_score;
+    let warren_score;
+    let mark_score;
+    let serena_score;
     
     const generateResponse = async(judge) => {
       console.log(judge)
@@ -62,53 +67,47 @@ export function renderScreen3(pitch, category, renderScreen4) {
       });
 
       const data = await response.json()
-      return data
+      return data.response
+    };
+
+    const generateScore = async(judge, tweet) => {
+      console.log(judge)
+      const response = await fetch("http://localhost:3005/score", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ auto_pitch: auto_pitch, category: category, pitch: pitch, judge: judge, tweet: tweet})
+      });
+
+      const data = await response.json()
+      return data.response
     };
 
     const allJudgeResponses = async() => {
       elon = await generateResponse("Elon Musk");
+      console.log(elon)
       beyonce = await generateResponse("Beyonce");
       warren = await generateResponse("Warren Buffett");
       mark = await generateResponse("Mark Cuban");
       serena = await generateResponse("Serena Williams");
+
+      elon_score = await generateScore("Elon Musk", elon) 
+      console.log(elon_score)
+      beyonce_score = await generateScore("Beyonce", beyonce)  
+      warren_score = await generateScore("Warren Buffett", warren)  
+      mark_score = await generateScore("Mark Cuban", mark)
+      serena_score = await generateScore("Serena Williams", serena)
     }
 
     allJudgeResponses()
-
-    console.log("elon")
-    console.log(elon)
-    console.log(beyonce)
-
-
-    // const generateScore = async(judge, tweet) => {
-    //   const response = await fetch("http://localhost:3005/score", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({ auto_pitch: auto_pitch, category: category, pitch: pitch, judge: judge, tweet: tweet})
-    //   });
-
-    //   const data = await response.json()
-    //   return data
-    // };
-
-    // elon_score = generateScore("Elon Musk", elon) 
-    // beyonce_score = generateScore("Beyonce", beyonce)  
-    // warren_score = generateScore("Warren Buffett", warren)  
-    // mark_score = generateScore("Mark Cuban", mark)
-    // serena_score = generateScore("Serena Williams", serena)
-
-    
-
-    // generateScores()
-    //   .then(() => {
-    //         for (let i = 0; i < elements.length; i++) {
-    //           const element = elements[i];
-    //           element.style.display = "none"
-    //         }
-    //         musicPlayer.remove();
-    //     renderScreen4(elon, elon_score, beyonce, beyonce_score, warren, warren_score, mark, mark_score, serena, serena_score, pitch)
-    // })
+      .then(() => {
+            for (let i = 0; i < elements.length; i++) {
+              const element = elements[i];
+              element.style.display = "none"
+            }
+            musicPlayer.remove();
+        renderScreen4(elon, elon_score, beyonce, beyonce_score, warren, warren_score, mark, mark_score, serena, serena_score, pitch)
+    })
   
   }
